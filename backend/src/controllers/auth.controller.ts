0162f3b978390f13d8cd923a0b3e7f6651fa4d4b
MANPREETS47 <manpreetsinghms1080@gmail.com>
2025-08-13 23:42:42 +0530

    Worked today

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,8 +2,6 @@ import {Request, Response} from "express";
 
 
 
-const signup = (req: Request, res: Response) => {
+export const signup = (req: Request, res: Response) => {
     // Handle signup logic
 };
-
-export default signup;
